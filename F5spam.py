8e4ac9df433333d48a31f2fb32c3ee6108bf24b5
Francesco Marchiori <73738800+FrancescoMarchiori@users.noreply.github.com>
2022-06-24 17:17:05 +0200

    [➕] Added multiple URLs

--- F5spam.py
+++ F5spam.py
@@ -1,7 +1,24 @@
+from multiprocessing.pool import TERMINATE
 import time
 import hashlib
 from urllib.request import urlopen, Request
 from plyer import notification
+import sys
+
+def sanitize(url_input):
+    """
+    Given a url (str), sanitize it and make it processable
+    """
+
+    # Sanitizing the input (might not include https://)
+    if url_input[:8] != 'https://':
+        if url_input[:4] == 'www.':         # Input: www.google.com
+            url_input = url_input[4:]
+            url_input = 'https://' + url_input
+        elif url_input[:7] != 'http://':                               # Input: google.com
+            url_input = 'https://' + url_input
+
+    return url_input
 
 print()
 print('###############################################################')
@@ -19,32 +36,55 @@ print(r'                               888')
 print()
 print('###############################################################')
 
+# Asking how many urls to search
+while True:
+    try:
+        num_input = int(input('\n[🔢] How many URL do you want to check? Type here: '))
+        break
+    except Exception:
+        print('[❌ ERROR] Not an integer number')
 
 # Asking the URL to check
-url_input = input('\n[🌐] What URL do you want to check? Type here: ')
+if num_input == 1:
+    url_input = input('[🌐] What URL do you want to check? Type here: ')
+else:
+    url_input = []
+    for i in range(num_input):
+        this_url = input(f'[🌐] URL #{i+1}: ')
+        url_input.append(this_url)
 
 # Loop to trap user until the URL is correct
 while True:
-    # Sanitizing the input (might not include https://)
-    if url_input[:8] != 'https://':
-        if url_input[:4] == 'www.':         # Input: www.google.com
-            url_input = url_input[4:]
-            url_input = 'https://' + url_input
-        elif url_input[:7] == 'http://':    # Input: http://.google.com
-            url_input = url_input[7:]
-            url_input = 'https://' + url_input
-        else:                               # Input: google.com
-            url_input = 'https://' + url_input
-    
-    # Requesting the url
-    url = Request(url_input, headers={'User-Agent': 'Mozilla/5.0'})
-    
-    # Reading the response
-    try:
-        response = urlopen(url).read()
+    if type(url_input) != list:
+        url_input = sanitize(url_input)
+
+        # Requesting the url
+        url = Request(url_input, headers={'User-Agent': 'Mozilla/5.0'})
+        
+        # Reading the response
+        try:
+            response = urlopen(url).read()
+            break
+        except Exception:
+            url_input = input('[❌ ERROR] Something went wrong. Type the URL again: ')
+    else:
+        urls = []
+        responses = []
+        for this_url in url_input:
+            this_url = sanitize(this_url)
+        
+            # Requesting the url
+            url = Request(this_url, headers={'User-Agent': 'Mozilla/5.0'})
+            urls.append(url)
+            
+            # Reading the response
+            try:
+                response = urlopen(url).read()
+                responses.append(response)
+            except Exception:
+                print('[❌ ERROR] Something went wrong')
+                sys.exit()
         break
-    except Exception:
-        url_input = input('[❌ ERROR] Something went wrong. Type the URL again: ')
 
 # Asking how often to check
 time_input = input('[⏰] How often do you want to check? Type in seconds: ')
@@ -57,44 +97,85 @@ while True:
         time_input = input('[❌ ERROR] Something went wrong. Please type an integer value: ')
  
 # Using the hash to monitor changes 
-currentHash = hashlib.sha224(response).hexdigest()
+if num_input == 1:
+    currentHash = hashlib.sha224(response).hexdigest()
+else:
+    currentHashes = []
+    for response in responses:
+        currentHash = hashlib.sha224(response).hexdigest()
+        currentHashes.append(currentHash)
+
 print("\n[🚀 RUNNING]")
 
 # Keeping track of the number of times checked
 i = 1
 while True:
     print(f'\t[⏱️ TIMES CHECKED] {i}', end='\r')
-    try:
-        # Reading the response
-        response = urlopen(url).read()
-         
-        # Create current hash
-        currentHash = hashlib.sha224(response).hexdigest()
-         
-        # Waiting for the set amount of time
-        time.sleep(time_input)
-         
-        # Reading the response again
-        response = urlopen(url).read()
-         
-        # Create a new hash
-        newHash = hashlib.sha224(response).hexdigest()
- 
-        # Check whether the hash has changed or not
-        if newHash == currentHash:
-            i += 1
-            continue
-        else:
-            print("\t[✅ CHANGE DETECTED]")
-            # Giving a notification to the user
-            notification.notify(
-                title = '[✅ CHANGE DETECTED]',
-                message = f'Something changed in "{url_input}", go check it out!',
-                app_icon = './imgs/icon.ico',
-                timeout = 10
-            )
+    if num_input == 1:
+        try:
+            # Reading the response
+            response = urlopen(url).read()
+            
+            # Create current hash
+            currentHash = hashlib.sha224(response).hexdigest()
+            
+            # Waiting for the set amount of time
+            time.sleep(time_input)
+            
+            # Reading the response again
+            response = urlopen(url).read()
+            
+            # Create a new hash
+            newHash = hashlib.sha224(response).hexdigest()
+    
+            # Check whether the hash has changed or not
+            if newHash == currentHash:
+                i += 1
+                continue
+            else:
+                print("\t[✅ CHANGE DETECTED]")
+                # Giving a notification to the user
+                notification.notify(
+                    title = '[✅ CHANGE DETECTED]',
+                    message = f'Something changed in "{url_input}", go check it out!',
+                    app_icon = './imgs/icon.ico',
+                    timeout = 10
+                )
+                break
+        # Handle exceptions
+        except Exception:
+            print("[❌ ERROR] An error occured")
             break
-    # Handle exceptions
-    except Exception:
-        print("[❌ ERROR] An error occured")
-        break
+    else:
+        try:
+            for response in responses:
+                # Reading the response
+                response = urlopen(url).read()
+                
+                # Create current hash
+                currentHash = hashlib.sha224(response).hexdigest()
+                
+                # Reading the response again
+                response = urlopen(url).read()
+                
+                # Create a new hash
+                newHash = hashlib.sha224(response).hexdigest()
+        
+                # Check whether the hash has changed or not
+                if newHash != currentHash:
+                    print("\t[✅ CHANGE DETECTED]")
+                    # Giving a notification to the user
+                    notification.notify(
+                        title = '[✅ CHANGE DETECTED]',
+                        message = f'Something changed in "{url_input}", go check it out!',
+                        app_icon = './imgs/icon.ico',
+                        timeout = 10
+                    )
+                    sys.exit()
+            # Waiting for the set amount of time
+            time.sleep(time_input)
+            i += 1
+        # Handle exceptions
+        except Exception:
+            print("[❌ ERROR] An error occured")
+            break
